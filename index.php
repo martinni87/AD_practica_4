fee687d92331e1fa9b670805de4d39998657219c
Martin Antonio <mancorge@gmail.com>
2023-01-18 10:52:46 +0100

    Actualizaciones en estilos en todas las vistas

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <script src="vendor/jquery-3.6.1.min.js"></script>
     <script src="controller/medicos.js"></script>
-    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="./css/login_styles.css">
     
 </head>
 <body>
